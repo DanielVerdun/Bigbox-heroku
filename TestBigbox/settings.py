f5e0aebfc4862e1887a138a18585b913c1c383c0
DanielVerdun <verdundaniel20@gmail.com>
2021-05-05 03:53:55 -0300

    voviendo a configuracion anterior

--- TestBigbox/settings.py
+++ TestBigbox/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'TestBigbox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bigbox',
-        'USER': 'daniel',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   #     'NAME': 'bigbox',
+    #    'USER': 'daniel',
+     #   'PASSWORD': '123456',
+      #  'HOST': 'localhost',
+       # 'PORT': '5432',
+    #}
+#}
 import dj_database_url
 from decouple import config
 
-#DATABASES = {
- #   'default':dj_database_url.config(default=config('DATABASE_URL'))
-#}
+DATABASES = {
+    'default':dj_database_url.config(default=config('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
