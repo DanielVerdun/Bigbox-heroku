00ef5b9afb0e815666a4f7a2f94906a9445236e7
DanielVerdun <verdundaniel20@gmail.com>
2021-05-05 01:13:19 -0300

    Creando repo de TestBigbox en heroku

---
+++ bigbox/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from bigbox.models import Category,Reason,Activity,Box
+
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Reason)
+admin.site.register(Activity)
+admin.site.register(Box)
---
+++ bigbox/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from bigbox.models import Activity, Box,Prodcut,Category
+
+# Create your views here.
+
+
+def box(request):
+    # Todos los objetos  de la class Box, lo guardamos dentro de la variable.
+    boxs = Box.objects.all()
+    # Luego mostramos los datos en box.html
+    return render(request, "box.html", {"boxs": boxs})
+
+def infobox(request, category_id):
+    # filtra la box que reciba en el parametro categoria_id
+    boxs = Box.objects.filter(id=category_id )
+    # Muestra las primeras cinco actividades
+    activities= Activity.objects.all()[:5]
+    return render(request, "infobox.html", {"boxs": boxs,"activities":activities})
+
+def activity(request):
+    #Muestra todas las actividades    
+    activities= Activity.objects.all()
+    return render(request, "activity.html",{"activities":activities})
