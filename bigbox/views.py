b970d358f80fb350dec9c3956e5bffeb80ce9e4b
DanielVerdun <verdundaniel20@gmail.com>
2021-05-17 02:00:09 -0300

    cambios

--- bigbox/views.py
+++ bigbox/views.py
@@ -23,7 +23,7 @@ def infobox(request, category_id):
 def activity(request):
     # Muestra todas las actividades /incluye paginador   
     
-    #actividades_id =Activity.objects.filter()
+    activities= Activity.objects.all()
     boxs = Box.objects.all()
     actividades= Activity.objects.all()
     paginator = Paginator(actividades,20)
@@ -31,4 +31,4 @@ def activity(request):
     page = request.GET.get("page",1)
     actividades_page = paginator.get_page(page)           
     
-    return render(request, "activity.html",{"actividades":actividades,"actividades_page":actividades_page,"boxs": boxs})
+    return render(request, "activity.html",{"actividades":actividades,"actividades_page":actividades_page,"boxs": boxs,"activities":activities})
