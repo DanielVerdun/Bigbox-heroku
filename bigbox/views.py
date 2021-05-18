c9d781873a2637f260b3c993080f2571afe85736
DanielVerdun <verdundaniel20@gmail.com>
2021-05-18 01:21:50 -0300

    comabios

--- bigbox/views.py
+++ bigbox/views.py
@@ -1,32 +1,41 @@
 from django.shortcuts import render
-from bigbox.models import Activity, Box,Prodcut,Category
+from bigbox.models import Activity, Box, Prodcut, Category
 from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def box(request):
+
     # Todos los objetos  de la class Box, lo guardamos dentro de la variable.
     boxs = Box.objects.all()
     # Luego mostramos los datos en box.html
-    
+
     return render(request, "box.html", {"boxs": boxs})
 
+
 def infobox(request, category_id):
+
     # filtra la box que reciba en el parametro categoria_id
-    boxs = Box.objects.filter(id=category_id )
+    boxs = Box.objects.filter(id=category_id)
     # Muestra las primeras cinco actividades
-    activities= Activity.objects.all()[:5]
-    
-    return render(request, "infobox.html", {"boxs": boxs,"activities":activities})
+    activities = Activity.objects.all()[:5]
+    boxes = Box.objects.all()
+    return render(request, "infobox.html", {"boxs": boxs, "activities": activities,"boxes":boxes})
+
 
 def activity(request):
-    # Muestra todas las actividades /incluye paginador 
-    boxs =Box.objects.all()
-    actividades= Activity.objects.all()
-    paginator = Paginator(actividades,20)
-    page = request.GET.get("page",1)
+    
+    # Muestra todas las actividades /incluye paginador
+    
+    actividades = Activity.objects.all()
+    paginator = Paginator(actividades, 20)
+    page = request.GET.get("page", 1)
     actividades_page = paginator.get_page(page)
+        
+    return render(request, "activity.html", {"actividades": actividades, "actividades_page": actividades_page, "boxs": boxs})
 
-    return render(request, "activity.html",{"actividades":actividades,"actividades_page":actividades_page,"boxs": boxs})
+  
+
+
     
